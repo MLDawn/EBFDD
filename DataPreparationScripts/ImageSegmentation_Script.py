503c6f3b899787d61ecc9e2d1406cdddbf625cf7
MLDawn <43797799+MLDawn@users.noreply.github.com>
2019-03-27 14:50:34 +0000

    Update ImageSegmentation_Script.py

--- DataPreparationScripts/ImageSegmentation_Script.py
+++ DataPreparationScripts/ImageSegmentation_Script.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def prepare_imagesegmentation(normal, anomalous):
     # Depending on what normal is, build the anomalous label's list
-    dataset = pd.read_csv('D:\PhD\AD-Benchmark Datasets\Multiclass Classification\\imgseg.original.csv')
+    dataset = pd.read_csv('imgseg.original.csv')
     X = dataset[dataset.columns[1:]]
     X = X.values
     Y = np.array(dataset["Y"])
