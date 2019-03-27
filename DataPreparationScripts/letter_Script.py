7dad2499ce08eaf41a3b1471f4bd79f6ecda2901
MLDawn <43797799+MLDawn@users.noreply.github.com>
2019-03-27 14:52:59 +0000

    Update letter_Script.py

--- DataPreparationScripts/letter_Script.py
+++ DataPreparationScripts/letter_Script.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def prepare_letter(normal, anomalous):
     # Depending on what normal is, build the anomalous label's list
-    dataset = pd.read_csv('D:\PhD\AD-Benchmark Datasets\Multiclass Classification\\letter.rec.original.csv')
+    dataset = pd.read_csv('letter.rec.original.csv')
     X = dataset[dataset.columns[1:]]
     X = X.values
     Y = np.array(dataset["Y"])
